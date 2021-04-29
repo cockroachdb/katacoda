d34641837ea1bd4da6d180ee2011fccf52f1bc07
Jesse Seldess <jesse@cockroachlabs.com>
2021-04-29 15:40:52 -0400

    Use background script for socat

---
+++ multi-region/background.sh
@@ -0,0 +1,1 @@
+socat TCP4-LISTEN:8081,fork,reuseaddr TCP4:127.0.0.1:8080
--- multi-region/foreground.sh
+++ multi-region/foreground.sh
@@ -4,5 +4,4 @@ mkdir -p /usr/local/lib/cockroach
 cp -i cockroach-v21.1.0-beta.4.linux-amd64/lib/libgeos.so /usr/local/lib/cockroach/
 cp -i cockroach-v21.1.0-beta.4.linux-amd64/lib/libgeos_c.so /usr/local/lib/cockroach/
 docker pull cockroachdb/movr:20.11.1
-socat TCP4-LISTEN:8081,fork,reuseaddr TCP4:127.0.0.1:8080 &
 exec cockroach demo --global --nodes 9 --empty --insecure
